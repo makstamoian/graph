5e8c2cf3ae4da57184fe0bd5e902b51b75c719ce
maxtamoian <maks.tamoian@gmail.com>
2023-10-26 14:44:40 +0200

    cover functions with test

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,23 @@ impl Graph {
         return nodes;
     }
 
-    fn dfs (&self, node: u32, visited_nodes: &mut HashSet<u32>) {
+    fn has_node(&self, node: u32) -> bool {
+        return self.nodes.contains_key(&node)
+    }   
+
+    fn has_edge(&self, node_a: u32, node_b: u32) -> bool {
+        if self.has_node(node_a) && self.has_node(node_b) {
+            return self.nodes[&node_a].contains(&node_b) && self.nodes[&node_b].contains(&node_a)
+        }
+
+        return false;
+    }
+
+    fn get_node_adjacents(&self, node: u32) -> &HashSet<u32>{
+        return &self.nodes[&node];
+    }
+
+    fn depth_first_search (&self, node: u32, visited_nodes: &mut HashSet<u32>) {
         visited_nodes.insert(node);
 
         for new_node in &self.nodes[&node]{
@@ -49,8 +65,14 @@ impl Graph {
                 continue;
             }
 
-            self.dfs(*new_node, visited_nodes)
+            self.depth_first_search(*new_node, visited_nodes)
         }
+    }
+
+    fn clear (&mut self) -> &HashMap<u32, HashSet<u32>>{
+        let _ = &self.nodes.clear();
+
+        return &self.nodes;
     }
 }
 
@@ -76,13 +98,90 @@ fn generate_graph(nodes_count: u32, edges_count: u32) -> Graph {
 }
 
 fn main() {
-    let my_graph = generate_graph(10, 11);
+    let mut my_graph = generate_graph(10, 11);
 
     let mut visited_nodes = HashSet::new();
 
-    my_graph.dfs(0, &mut visited_nodes);
+    my_graph.depth_first_search(0, &mut visited_nodes);
 
     println!("Visited total of {} nodes:\n{:#?}", visited_nodes.len(), visited_nodes);
-    println!("{:#?}", my_graph.get_nodes());
+
+    println!("List of all node ids in generated graph:\n{:#?}", my_graph.get_nodes());    
+
+    println!("Check if generated graph contains node with id 1: {}", my_graph.has_node(1));
+
+    println!("{:#?}", my_graph.get_node_adjacents(2));
+
+    my_graph.clear();
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_graph_add_node() {
+        let mut graph = Graph { nodes: HashMap::new() };
+
+        graph.add_node(1);
+        graph.add_node(2);
+        graph.add_node(3);
+
+        assert_eq!(graph.get_nodes(), HashSet::from([1, 2, 3]));
+    }
+
+    #[test]
+    fn test_graph_add_edge() {
+        let mut graph = Graph { nodes: HashMap::new() };
+
+        graph.add_node(1);
+        graph.add_node(2);
+        graph.add_node(3);
+
+        graph.add_edge(1, 2);
+        graph.add_edge(2, 3);
+        graph.add_edge(3, 2);
+
+        assert_eq!(graph.has_edge(1, 2), true);
+    }
+
+    #[test]
+    fn test_graph_clear() {
+        let mut graph = Graph { nodes: HashMap::new() };
+
+        graph.add_node(1);
+        graph.add_node(2);
+        graph.add_node(3);
+
+        graph.add_edge(1, 2);
+        graph.add_edge(2, 3);
+        graph.add_edge(3, 2);
+        
+        graph.clear();
+
+        assert_eq!(graph.get_nodes(), HashSet::from([]));
+    }
+
+    #[test]
+    fn test_graph_has_node() {
+        let mut graph = Graph { nodes: HashMap::new() };
+
+        graph.add_node(1);
+
+        assert_eq!(graph.has_node(1), true);
+    }
+
+    #[test]
+    fn test_graph_has_edge() {
+        let mut graph = Graph { nodes: HashMap::new() };
+
+        graph.add_node(1);
+        graph.add_node(2);
+        graph.add_node(3);
+
+        graph.add_edge(1, 2);
+        graph.add_edge(2, 3);
+        graph.add_edge(3, 2);
+        
+        assert_eq!(graph.has_edge(1, 2), true);
+    }
+}
